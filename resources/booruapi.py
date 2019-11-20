7305d8a4377f1613a36c0716dfb529618383af17
ohaui <42948516+ohaui@users.noreply.github.com>
2019-11-20 10:39:12 +0300

    Update booruapi.py

--- resources/booruapi.py
+++ resources/booruapi.py
@@ -1,16 +1,15 @@
 from __future__ import unicode_literals
 from pybooru import Danbooru
-from pybooru.exceptions import PybooruHTTPError
 import discord
 
 #TODO разобраться почему запрос score:1000 занимает так много времени
-rd = open("resources/DANBOORUTOKEN.txt", "r")
+rd = open("resources/DANBOORUTOKEN.txt", "r") #никнейм и токен на данбоору
 tokens = rd.readlines()
 booru = client = Danbooru('danbooru', username=tokens[0], api_key=tokens[2])
 def getLinkBoorus(tags):
         getpicture = booru.post_list(tags=tags, random=True, limit = 1)
         #tags - тэги для поиска, random = рандомная ли картинка, limit - лимит постов
-        returns = getpicture[0].get('file_url'), getpicture[0].get('score'),\
+        to_return = getpicture[0].get('file_url'), getpicture[0].get('score'),\
                   getpicture[0].get('tag_string_artist')
-        print(returns)
-        return returns
+        print(to_return) # отображает ссылку, рейтинг и художника рандомного поста
+        return to_return
