10c31c050343e7bdbc6c52c1e2a38a8ae97fd8f0
ohaui <exetwinner@gmail.com>
2019-11-25 04:42:11 +0300

    перенес котов на imgur, вроде все работает

--- resources/random_cat.py
+++ resources/random_cat.py
@@ -1,12 +1,18 @@
 import requests
 import json
-import os
 import random
-import sys
-from PIL import Image, ImageFilter
+from random import randint
+from imgurpython import ImgurClient
 
-myCatPicDir = 'myCatPics'
-# путь к папке с котами
+
+with open("resources/IMGURTOKEN.txt", "r") as token: #imgur токен
+    readtoken = token.readlines()
+    client_id = readtoken[0] # почему-то выдает "тут токен"\n
+    client_id = client_id.replace('\n', '') # без этой комманды ничего не работает
+    client_secret = readtoken[2]
+
+imgur = ImgurClient(client_id, client_secret)
+
 
 def getCat():
     response = requests.get('https://api.thecatapi.com/v1/images/search')
@@ -16,12 +22,20 @@ def getCat():
     CatUrl = pyDictionary.get('url')
     return CatUrl
 
-def myGetCat():
-    global randomCatPic
-    PicDict = os.listdir(myCatPicDir)
-    randomCatPic = (random.choice(PicDict))
-    return randomCatPic
+def init_myCatImages():
+    global readline
+    with open('resources/links.txt', "r") as link:
+        readline = link.readlines()
+        album = imgur.get_album_images("0m0etMm")
+        with open('resources/links.txt', "w") as link:
+        #пришлось открывать файл снова, потому что иначе работает как говно!
+            for images in album:
+                link.write(images.link + '\n')
+    return '{} - number of cats!'.format(len(readline))
 
-def myGetlen():
-    PicDict = os.listdir(myCatPicDir)
-    return len(PicDict)
+def lenMyCatImages():
+    return '{} - котов в альбоме'.format(len(readline))
+
+def getMyCatImages():
+    randomNumber = randint(0, len(readline))
+    return readline[randomNumber]
